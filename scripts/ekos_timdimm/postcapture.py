c24634f72ffbdfe1488399aefd12665256806764
T. E. Pickering <te.pickering@gmail.com>
2025-02-17 12:20:10 -0700

    tweak exposure times

--- scripts/ekos_timdimm/postcapture.py
+++ scripts/ekos_timdimm/postcapture.py
@@ -65,13 +65,14 @@ x, y = np.mean(ap_stats.centroid, axis=0)
 
 # center apertures in a 400x400 ROI and grab a 15 second cube
 if ap_stats.max.min() > 8000:
-    exptime = 0.0002
+    exptime = 0.0001
 elif ap_stats.max.min() > 4000:
-    exptime = 0.0005
+    exptime = 0.0002
 elif ap_stats.max.min() < 500:
     exptime = 0.002
 else:
-    exptime = 0.001
+    exptime = 0.0005
+
 cam.stream_exposure(exptime)
 left = max(0, int(x - 200))
 top = max(0, int(y - 200))
